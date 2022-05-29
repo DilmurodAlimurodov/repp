dec07dbe0e4c96c75d1d13e48197bdb818479e8a
Manuchehr <manuchekhr.3232@gmail.com>
2022-05-30 00:35:14 +0500

    Deployment

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,17 +1,17 @@
 import { Sequelize } from 'sequelize-typescript'
 import * as models from '../models'
 
-const connection = new Sequelize('postgres', 'postgres', '2004', {
+const connection = new Sequelize('d172m3vvm1p2i4', 'mkenisucqnunnd', 'f85a26d92439c0edb43c84cd70aae4e4c74fa7e1197301048a5f922154a083c9', {
   dialect: 'postgres',
-  host: 'localhost',
-  port: 1234,
+  host: 'ec2-44-196-174-238.compute-1.amazonaws.com',
+  port: 5432,
   models: [models.default.Book, models.default.Category],
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true, 
-  //     rejectUnauthorized: false 
-  //   }
-  // },
+  dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false 
+    }
+  },
 })
 
 export default connection
